2d4045bb47ad8af4716d292c3d522cacd4fe1580
Vitor Reis <vitinhoreis218@gmail.com>
2022-05-09 21:17:31 -0300

    Atividade1

---
+++ cadastro-pecas.js
@@ -0,0 +1,14 @@
+let listaPecas = 3;
+let peso = 150;
+let nomePeca = "cilindro mestre"
+
+if(peso >= 100 ){
+    console.log("peso suficiente");
+    if(listaPecas >=10){
+        console.log("lista cheia, excedeu a capacidade");
+    }
+
+    if(nomePeca.length <3){
+        console.log("nome muito curto");
+    }
+}
